4fc4c5277bf14e2d100027c8f9b05b8aee2ad74e
show0017 <show0017@algonquinlive.com>
2015-12-31 14:05:24 -0500

    Fix racing condition that occurs while outer scroll view triggers its inner scroll view about threshold crossing/return. Change backgrounds of all views to be white

--- iKea Shopping List/views/ShoppingListsTableView.m
+++ iKea Shopping List/views/ShoppingListsTableView.m
@@ -22,11 +22,23 @@
 
 - (void)scrollViewDidScroll{
   if((self.contentOffset.y > SCROLLING_THRESHOLD) && (YES == self.shouldNotifyDelegate)){
-    [self.scrollingDelegate scrollViewDidCrossOverThreshold:self];
+
+    /* It is mandatory to set the this notification flag before calling the delegate because
+     * the delegate object might take longer times for execution then it will be preempted
+     * and this inner srollview would trigger this method again and the flag is not toggeled yet
+     * hence the delegate method will be triggered again which results in transforming & scaling
+     * the sticky view more than required. */
     self.shouldNotifyDelegate = NO;
-  }else if((self.contentOffset.y < SCROLLING_THRESHOLD) && (NO == self.shouldNotifyDelegate)){
-    [self.scrollingDelegate scrollViewDidReturnBelowThreshold:self];
+    [self.scrollingDelegate scrollViewDidCrossOverThreshold:self];
+  }else if((self.contentOffset.y < SCROLLING_THRESHOLD) &&(NO == self.shouldNotifyDelegate)){
+    
+    /* It is mandatory to set the this notification flag before calling the delegate because
+     * the delegate object might take longer times for execution then it will be preempted
+     * and this inner srollview would trigger this method again and the flag is not toggeled yet
+     * hence the delegate method will be triggered again which results in decreasing the height
+     * of the inner scroll view more than needed. */
     self.shouldNotifyDelegate = YES;
+    [self.scrollingDelegate scrollViewDidReturnBelowThreshold:self];
   }
 }
 
